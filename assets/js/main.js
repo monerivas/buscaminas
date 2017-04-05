f2362559dd4d0fea529970a31c589ee2555b570f
Mone Rivas <mone.rivas@gmail.com>
2017-04-05 08:58:08 -0500

    Pura maquetacion

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+var elementos= document.getElementsByName("celda");
+
+function bombas() {
+  
+
+}
+
+function vacio() {
+  var celda1= document.getElementById("celda1");
+  celda1.style.backgroundColor= "#DE200D";
+}
+/*var celda1= document.getElementById("celda1");
+celda1.addEventListener("click", bombas);*/
